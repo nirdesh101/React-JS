865e1468cf9888b070dc1cb107df8fb7a66d87a5
nirdeshpokharel <nirdeshpokharel101gmail.com>
2023-05-01 17:42:13 +0545

    todo complete

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
-import Button from 'react-bootstrap/Button';
+import logo from "./logo.svg";
+import Button from "react-bootstrap/Button";
 // import "bootstrap/dist/css/bootstrap.min.css";
-import Header from './Components/Header'
-import Todo from './Components/Todo';
-
+import Header from "./Components/Header";
+import Todo from "./Components/Todo";
 
 function App() {
-
   return (
     <>
       <div>
-        <Header/>
-        <Todo  />  
-
+        <Header />
+        <Todo />
       </div>
     </>
   );
